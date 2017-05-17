755b340612ef5d23154e9f7d15aff5a30291a225
Saulius Zukauskas <saulius.zukauskas@gmail.com>
2017-05-17 20:07:53 +0100

    changing to headless browser for testing

--- karma.conf.js
+++ karma.conf.js
@@ -14,13 +14,14 @@ module.exports = function(config){
 
     autoWatch : true,
     frameworks: ['jasmine'],
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'
+      'karma-junit-reporter',
+      'karma-phantomjs-launcher'
     ],
 
     junitReporter : {
